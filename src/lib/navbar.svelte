a382384a727b15ac8167e986591af66265d32a7a
Zeng Xianhan <zengxianhan93@gmail.com>
2023-08-25 11:04:32 +0800

    Change sidebar and navbar, add bootstrap icon in package Add chat sidebar ui (WIP)

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -6,13 +6,13 @@
   
 </script>
 <div>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand md-2" href="{base}/">MSE Teams</a>
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand md-2" href="{base}/">Team 8 PM Project</a>
     <!-- search function, move to component when ready -->
-    <form class="form-inline mx-auto">
+    <!-- <form class="form-inline mx-auto">
         <input class="form-control" type="text" placeholder="Search">
-    </form>
-    <div class="justify-content-md-right">
+    </form> -->
+    <div class="nav justify-content-end">
       <UserOptions {user}/>
     </div>
     
--- src/lib/sidebar.svelte
+++ src/lib/sidebar.svelte
@@ -1,34 +1,26 @@
 <script>
   import { base } from '$app/paths'; // for github pages
-  import Home from "svelte-material-icons/Home.svelte";
-  import MessageText from "svelte-material-icons/MessageText.svelte";
-  import CardsPlayingSpadeMultiple from "svelte-material-icons/CardsPlayingSpadeMultiple.svelte"
+  import Bell from "svelte-bootstrap-icons/lib/Bell.svelte";
+  import ChatDots from "svelte-bootstrap-icons/lib/ChatDots.svelte";
+
   //icon options
   export let size = "2em"; // string | number
   export let width = size; // string | number
   export let height = size; // string | number
-  export let color= "black";
+  export let color = "primary";
 </script>
-
-
 
 <div class="d-flex flex-column flex-shrink-0 bg-light" style="width: 4.5rem;">
     <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-      <li class="nav-item">
-    <a href="{base}/" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
-      <Home  {size} {width} {height} {color}/>
-    </a>
-      </li>
       <li>
-        <a href="{base}/chat" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Chat">
-          <MessageText {size} {width} {height} {color}/>
+        <a href="{base}/" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Chat">
+          <ChatDots {size} {width} {height} {color}/>
         </a>
       </li>
       <li>
         <a href="{base}/quiz" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="quiz">
-          <CardsPlayingSpadeMultiple  {size} {width} {height} {color}/>
+          <Bell {size} {width} {height} {color}/>
         </a>
       </li>
-
     </ul>
   </div>
