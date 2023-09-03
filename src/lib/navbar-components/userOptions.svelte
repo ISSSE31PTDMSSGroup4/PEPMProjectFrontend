c534a7c34ce1eac9357241b0b63d38b1b9e7ae64
redthorx <55249192+redthorx@users.noreply.github.com>
2023-09-03 16:31:20 +0800

    Update userOptions.svelte
    Added {base} due to build error

--- src/lib/navbar-components/userOptions.svelte
+++ src/lib/navbar-components/userOptions.svelte
@@ -21,7 +21,7 @@
         </a>
       </div>
       <div class="col">
-        <a class="dropdown-toggle" href="/" data-toggle="dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <a class="dropdown-toggle" href="{base}/" data-toggle="dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           <img src={avartar} class="rounded-circle" style="width: {width}; size:{size}; width:{width};height:{height};" alt="Avatar" />
         </a>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink" >
