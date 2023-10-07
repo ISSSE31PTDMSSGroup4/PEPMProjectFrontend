50825bf0d24ba40c9bfa7c5e95e76a3faa4b7ec9
yj <yj>
2023-10-07 18:50:46 +0800

    changed base api

--- src/routes/constants.js
+++ src/routes/constants.js
@@ -2,9 +2,9 @@ import { base } from "$app/paths";
 //Team Postman Mock Server
 //export const baseApiUrl = "https://3a2e6f26-d322-46fe-a0b5-2dd5a314213d.mock.pstmn.io"; 
 //Personal Postman Mock Server
-export const baseApiUrl = "https://88847fa7-9974-44f1-b303-5e68a533bd65.mock.pstmn.io";
+//export const baseApiUrl = "https://88847fa7-9974-44f1-b303-5e68a533bd65.mock.pstmn.io";
 //Domain Server
-//export const baseApiUrl = "https://issse31ptdmss.xyz/api";
+export const baseApiUrl = "https://issse31ptdmss.xyz/api";
 
 export const userProfileUrl = "/user/profile";
 export const getUserQuizListUrl = "/quiz/list";
@@ -13,11 +13,11 @@ export const quizUrl = "/quiz";
 export const questionUrl = "/quiz/question";
 
 export const routeRoot = base + "/";
-export const routeQuiz = base +"/quiz";
-export const routeProfile = base +"/profile";
-export const routeAbout = base +"/about";
-export const routeLogin = "https://issse31ptdmss.xyz/api/auth/initiate_auth";
-export const routeLogout = base +"/logout";
+export const routeQuiz = base + "/quiz";
+export const routeProfile = base + "/profile";
+export const routeAbout = base + "/about";
+export const routeLogin = baseApiUrl + "/auth/initiate_auth/";
+export const routeLogout = baseApiUrl + "/auth/logout";
 
 export const viewMode = 1;
 export const editMode = 2;
