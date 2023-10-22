6799cec01e253e9c77bb4e50e0c26bf4bf084691
Zeng Xianhan <zengxianhan93@gmail.com>
2023-10-22 16:28:43 +0800

    Update url to dev env

--- src/routes/constants.js
+++ src/routes/constants.js
@@ -11,21 +11,21 @@ const userServiceBaseUrl = "http://localhost:8001/api";
 //const quizServiceBaseUrl = "https://88847fa7-9974-44f1-b303-5e68a533bd65.mock.pstmn.io";
 const quizServiceBaseUrl = "http://localhost:5050/api";
 //user profile microservice
-export const userProfileUrl = baseApiUrl + "/user/profile";
-export const updateUserProfileUrl = baseApiUrl + "/user/profile/update";
-export const userProfilesUrl = baseApiUrl + "/user/profiles";
-export const uploadAvatar = baseApiUrl + "/user/profile/avatar/upload";
+export const userProfileUrl = userServiceBaseUrl + "/user/profile";
+export const updateUserProfileUrl = userServiceBaseUrl + "/user/profile/update";
+export const userProfilesUrl = userServiceBaseUrl + "/user/profiles";
+export const uploadAvatar = userServiceBaseUrl + "/user/profile/avatar/upload";
 //quiz microservice
-export const getUserQuizListUrl = baseApiUrl + "/quiz/list";
-export const getQuizDetailUrl = baseApiUrl + "/quiz/detail";
-export const quizUrl = baseApiUrl + "/quiz";
-export const questionUrl = baseApiUrl + "/quiz/question";
+export const getUserQuizListUrl = quizServiceBaseUrl + "/quiz/list";
+export const getQuizDetailUrl = quizServiceBaseUrl + "/quiz/detail";
+export const quizUrl = quizServiceBaseUrl + "/quiz";
+export const questionUrl = quizServiceBaseUrl + "/quiz/question";
 //chat microservice
-export const pusherCfgUrl = baseApiUrl + "/chat/config/";
-export const chatMessageUrl = baseApiUrl + "/chat/message/";
-export const chatHisotyUrl = baseApiUrl + "/chat/history/";
-export const chatChannelUrl = baseApiUrl + "/chat/friendlist/";
-export const addChannelUrl = baseApiUrl + "/chat/addfriend/";
+export const pusherCfgUrl = chatServiceBaseUrl + "/chat/config/";
+export const chatMessageUrl = chatServiceBaseUrl + "/chat/message/";
+export const chatHisotyUrl = chatServiceBaseUrl + "/chat/history/";
+export const chatChannelUrl = chatServiceBaseUrl + "/chat/friendlist/";
+export const addChannelUrl = chatServiceBaseUrl + "/chat/addfriend/";
 
 //route
 export const routeRoot = base + "/";
@@ -33,8 +33,8 @@ export const routeQuiz = base +"/quiz";
 export const routeProfile = base +"/profile";
 export const routeAbout = base +"/about";
 //login microservice
-export const routeLogin = baseApiUrl + "/auth/initiate_auth";
-export const routeLogout = baseApiUrl + "/auth/logout";
+export const routeLogin = loginServiceBaseUrl + "/auth/initiate_auth";
+export const routeLogout = loginServiceBaseUrl + "/auth/logout";
 
 //Other constants
 export const viewMode = 1;
